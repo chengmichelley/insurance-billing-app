79470625be1205bcf330dcb111237d90c37324c4
Michelle Cheng <cheng.michelley@gmail.com>
2026-04-23 09:30:55 -0400

    Added Views Users to Manage User Role & Password Update

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,91 @@
 const express = require("express");
+const bcrypt = require("bcrypt")
 const router = express.Router();
 const User = require("../models/user");
 const authRequired = require("../middleware/authRequired");
+const isAdmin = require("../middleware/isAdmin");
 
-router.get("/all", authRequired, async (req, res) => {
+router.use(authRequired);
+
+// =======================
+// PUBLIC STAFF ROUTES
+// =======================
+
+router.get('/profile', async (req, res)=>{
+  try {
+    const user = await User.findById(req.session.user._id);
+    res.render("users/profile.ejs", { 
+      user, 
+      err: null,
+      message: req.query.message || null
+    });
+  } catch (error) {
+    res.render("error.ejs", { err: error.message });
+  }
+})
+
+router.put("/profile/password", async (req, res) => {
+  try {
+    const { password, confirmPassword } = req.body;
+    if(password !== confirmPassword) {
+      throw new Error('Passwords do not match!')
+    };
+
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    await User.findByIdAndUpdate(req.session.user._id, { hashedPassword })
+
+    res.redirect("/user/profile?message=Password updated successfully!");
+  } catch (error) {
+    const user = await User.findById(req.session.user._id);
+    res.render("users/profile.ejs", { user, err: error.message });
+  }
+});
+
+// =======================
+// ADMIN ONLY ROUTES
+// =======================
+
+router.use(isAdmin);
+
+// =======================
+// INDEX
+// =======================
+router.get("/", authRequired, isAdmin, async (req, res) => {
   try {
     const users = await User.find();
-    res.render("users/index", { users: users, currentUser: req.session.user });
+    res.render("users/index", { users });
+  } catch (error) {
+    res.render("error.ejs", { err: error.message });
+  }
+});
+
+// =======================
+// UPDATE
+// =======================
+router.put("/:id/role", async (req, res) => {
+  try {
+    if(req.params.id === req.session.user._id) {
+      throw new Error('Cannot change your own role.')
+    }
+
+    await User.findByIdAndUpdate(req.params.id, { role: req.body.role });
+    res.redirect('/user');
+  } catch (error) {
+    res.render("error.ejs", { err: error.message });
+  }
+});
+
+// =======================
+// DELETE
+// =======================
+router.delete("/:id", async (req, res) => {
+  try {
+    if(req.params.id === req.session.user._id) {
+      throw new Error('You cannot delete your own account.')
+    }
+
+    await User.findByIdAndDelete(req.params.id);
+    res.redirect('/user');
   } catch (error) {
     res.render("error.ejs", { err: error.message });
   }
