f3a53e588edf8a4398a4fcb728e6d2afe47963bd
Michelle Cheng <cheng.michelley@gmail.com>
2026-04-24 22:48:52 -0400

    Predeploy Update

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const authRequired = require('../middleware/authRequired')
 const isAdmin = require('../middleware/isAdmin')
 
